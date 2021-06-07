fb0a82711be8a8c8895d0c9039a189abac1adfa4
Paulo Lima <paulynho_lima@hotmail.com>
2021-06-06 21:03:16 -0300

    requisito 7 e 8 resolvidos

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 window.onload = function () {
 
 let cor = document.querySelector("#color-palette").firstElementChild;
-cor.className = "color cor1 selected";
-
-
+cor.className = "color cor1 selected ";
 
 
 
@@ -22,38 +20,25 @@ function changeColor() {
 let colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', changeColor);
 
-function paintPixel (){
-    
-    let black = document.getElementsByClassName('color');
-    let select = document.querySelector('.selected')
-    let color = document.querySelector('.cor1');
-    let color2 = document.querySelector('.cor2');
-    let color3 = document.querySelector('.cor3');
-    let color4 = document.querySelector('.cor4');
-
-    for(let index = 0;index < black.length;index+=1){
-        let result = black[index];
-    if(result === color && result === select){
-        let colorPixel = document.querySelector('.cor5');
-        colorPixel.classList.remove('cor5');
-        event.target.classList.add('cor1');
-    }else if(result === color2 && result === select){
-        let colorPixel = document.querySelector('.cor5');
-        colorPixel.classList.remove('cor5');
-        event.target.classList.add('cor2');
-    }else if(result === color3 && result === select){
-        let colorPixel = document.querySelector('.cor5');
-        colorPixel.classList.remove('cor5');
-        event.target.classList.add('cor3');
-    }else if(result === color4 && result === select){
-        let colorPixel = document.querySelector('.cor5');
-        colorPixel.classList.remove('cor5');
-        event.target.classList.add('cor4');
-    }
-
-    }
+function paintPixel (event){
+    let select = document.querySelector('.selected');
+    event.target.style.backgroundColor = window.getComputedStyle(select).backgroundColor; 
 }
 
-let enterColor = document.querySelector('#pixel-board');
-enterColor.addEventListener('click', paintPixel);
+let dom = document.getElementsByClassName('pixel')
+    for(let index2 = 0; index2 < dom.length; index2 +=1){
+        let dom2 = dom[index2];
+    dom2.addEventListener('click', paintPixel);
+    }
+
+function clear(){
+let clear2 = document.getElementsByClassName('pixel')
+for(let index3 = 0; index3 < clear2.length; index3 +=1){
+    let dom3 = clear2[index3];
+     dom3.removeAttribute('style');
+    }
+}
+let buton = document.querySelector('#clear-board');
+buton.addEventListener('click', clear);
+
 }
