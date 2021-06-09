2c4d049e2c597f2fc2b5a8ae39a424bb4e46d7a3
Paulo Lima <paulynho_lima@hotmail.com>
2021-06-09 01:24:35 -0300

    requisito 12 resolvido

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-window.onload = function () {
+window.onload = function() {
 
-let cor = document.querySelector("#color-palette").firstElementChild;
-cor.className = "color cor1 selected ";
+  const cor = document.querySelector('#color-palette').firstElementChild;
+    cor.className = 'color cor1 selected';
 
 
 
-let  pix = document.getElementsByClassName("pixel");
-for(let index = 0; index< pix.length;index+=1){
-    let bordas = pix[index];
-    bordas.className = " pixel borde cor5 "
-}
+  const pix = document.getElementsByClassName('pixel');
+    for(let index = 0; index < pix.length;index += 1) {
+    const bordas = pix[index];
+    bordas.className = 'pixel borde cor5'
+  }
 
-function changeColor() {
-    let selectedColor = document.querySelector('.selected');
+  function changeColor() {
+    const selectedColor = document.querySelector('.selected');
     selectedColor.classList.remove('selected');
     event.target.classList.add('selected');
-}
+  }
 
-let colorPalette = document.querySelector('#color-palette');
-colorPalette.addEventListener('click', changeColor);
-
-function paintPixel (event) {
+  let colorPalette = document.querySelector('#color-palette');
+    colorPalette.addEventListener('click', changeColor);
+  
+  function paintPixel (event) {
     let select = document.querySelector('.selected');
     event.target.style.backgroundColor = window.getComputedStyle(select).backgroundColor; 
-}
+  }
 
-let dom = document.getElementsByClassName('pixel')
+  const dom = document.getElementsByClassName('pixel')
     for(let index2 = 0; index2 < dom.length; index2 +=1) {
         let dom2 = dom[index2];
     dom2.addEventListener('click', paintPixel);
     }
 
-function clear() {
-let clear2 = document.getElementsByClassName('pixel')
-for(let index3 = 0; index3 < clear2.length; index3 +=1) {
+  function clear() {
+    let clear2 = document.getElementsByClassName('pixel')
+    for(let index3 = 0; index3 < clear2.length; index3 += 1) {
     let dom3 = clear2[index3];
      dom3.removeAttribute('style');
     }
-}
-let buton = document.querySelector('#clear-board');
-buton.addEventListener('click', clear);
+  }
+  const buton = document.querySelector('#clear-board');
+    buton.addEventListener('click', clear);
 
+  function  geraColor() {
+    let r = parseInt(Math.random() * 255);
+    let g = parseInt(Math.random() * 255);
+    let b = parseInt(Math.random() * 255);
+   
+    return `rgba(${r}, ${g}, ${b})`;
+  }
+  const gerar = document.getElementsByClassName('color');
+    for(let index4 = 1; index4 < gerar.length; index4 += 1) {
+    let ger = gerar[index4]
+    ger.style.backgroundColor = geraColor();
+   }
 }
