f5de24a1a0f64285c141a485688f2dcf7123dcef
Paulo Lima <paulynho_lima@hotmail.com>
2022-10-06 18:07:28 -0300

    feat add efcts in css

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-window.onload = function() {
-
+window.onload = function () {
   const cor = document.querySelector('#color-palette').firstElementChild;
-    cor.className = 'color cor1 selected';
-
-
+  cor.className = 'color cor1 selected';
 
   const pix = document.getElementsByClassName('pixel');
-    for(let index = 0; index < pix.length;index += 1) {
+  for (let index = 0; index < pix.length; index += 1) {
     const bordas = pix[index];
-    bordas.className = 'pixel borde cor5'
+    bordas.className = 'pixel borde cor5';
   }
 
   function changeColor() {
@@ -17,40 +14,61 @@ window.onload = function() {
     event.target.classList.add('selected');
   }
 
-  let colorPalette = document.querySelector('#color-palette');
-    colorPalette.addEventListener('click', changeColor);
-  
-  function paintPixel (event) {
-    let select = document.querySelector('.selected');
-    event.target.style.backgroundColor = window.getComputedStyle(select).backgroundColor; 
+  const colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', changeColor);
+
+  function paintPixel(event) {
+    const select = document.querySelector('.selected');
+    event.target.style.backgroundColor =
+      window.getComputedStyle(select).backgroundColor;
   }
 
-  const dom = document.getElementsByClassName('pixel')
-    for(let index2 = 0; index2 < dom.length; index2 +=1) {
-        let dom2 = dom[index2];
+  const dom = document.getElementsByClassName('pixel');
+  for (let index2 = 0; index2 < dom.length; index2 += 1) {
+    const dom2 = dom[index2];
     dom2.addEventListener('click', paintPixel);
-    }
+  }
 
   function clear() {
-    let clear2 = document.getElementsByClassName('pixel')
-    for(let index3 = 0; index3 < clear2.length; index3 += 1) {
-    let dom3 = clear2[index3];
-     dom3.removeAttribute('style');
+    const clear2 = document.getElementsByClassName('pixel');
+    for (let index3 = 0; index3 < clear2.length; index3 += 1) {
+      const dom3 = clear2[index3];
+      dom3.removeAttribute('style');
     }
+    window.location.reload();
   }
   const buton = document.querySelector('#clear-board');
-    buton.addEventListener('click', clear);
+  buton.addEventListener('click', clear);
 
-  function  geraColor() {
-    let r = parseInt(Math.random() * 255);
-    let g = parseInt(Math.random() * 255);
-    let b = parseInt(Math.random() * 255);
-   
+  function geraColor() {
+    const r = parseInt(Math.random() * 255);
+    const g = parseInt(Math.random() * 255);
+    const b = parseInt(Math.random() * 255);
+
     return `rgba(${r}, ${g}, ${b})`;
   }
   const gerar = document.getElementsByClassName('color');
-    for(let index4 = 1; index4 < gerar.length; index4 += 1) {
-    let ger = gerar[index4]
+  for (let index4 = 1; index4 < gerar.length; index4 += 1) {
+    const ger = gerar[index4];
     ger.style.backgroundColor = geraColor();
-   }
-}
+  }
+};
+
+const items = [
+  { name: 'Edward', value: 21 },
+  { name: 'Sharpe', value: 37 },
+  { name: 'And', value: 45 },
+  { name: 'The', value: -12 },
+  { name: 'Magnetic' },
+  { name: 'Zeros', value: 37 },
+];
+items.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
